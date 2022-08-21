975bf6c033988a347e0d95e776bc5f7619a9c503
sombriks <sombriks@gmail.com>
2022-08-20 23:00:40 -0300

    carga inicial

--- backend/src/main/resources/db/changelog/migrations/changeset_2022_08_20_22_36_29-carga_inicial.sql
+++ backend/src/main/resources/db/changelog/migrations/changeset_2022_08_20_22_36_29-carga_inicial.sql
@@ -13,6 +13,10 @@ values (1, 1, 'Prioritário'),
        (5, 3, 'Prioritário'),
        (6, 3, 'Entradas'),
        (7, 3, 'Saídas');
+insert into estados_documento (id, nome)
+values (1, 'novo'),
+       (2, 'transferido')
 
+-- rollback delete from estados_documento;
 -- rollback delete from pastas;
 -- rollback delete from setores;
