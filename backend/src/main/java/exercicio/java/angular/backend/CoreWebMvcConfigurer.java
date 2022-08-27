3f0a771d6316f22110f09b04cb9a47373984ae9d
Leonardo Silveira <sombriks@gmail.com>
2022-08-27 01:33:16 -0300

    adding cors config using WebMvcConfigurer

---
+++ backend/src/main/java/exercicio/java/angular/backend/CoreWebMvcConfigurer.java
@@ -0,0 +1,19 @@
+package exercicio.java.angular.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CoreWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+    }
+}
